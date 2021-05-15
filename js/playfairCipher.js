fa3549f3ab96c81cdbb1d161890b788387313d4d
rucha.patil18@vit.edu <rucha.patil18@vit.edu>
2021-05-15 07:48:38 +0530

    UI for playfair cipher

--- js/playfairCipher.js
+++ js/playfairCipher.js
@@ -139,7 +139,7 @@ function cipher() {
     enCodeStr = enCodeStr + coord1 + coord2;
   }
   document.getElementById("printValue").innerHTML = enCodeStr;
-  // alert("Добавили букву в середине слова? - " + flagAdd);
+  
   return enCodeStr;
 }
 
